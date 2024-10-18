8e0867957bc537641f5a046a68983af57c88bbe0
Federico Depetris <depetrisfe@gmail.com>
2024-10-18 13:24:26 +0200

    upload test from new device

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import MainBox from "./components/main-box";
 
 function App() {
+  //this is a test comment
   return (
     <div className={styles.main}>
       <MainBox />
